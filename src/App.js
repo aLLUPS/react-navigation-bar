de5d3b1922b6343e23e1700563e6c84f4d315a39
aLLUPS <73pawara@gmail.com>
2021-05-07 01:09:30 +0530

    completed navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <h2>project portfolio</h2>
+    <Navbar/>
+    
   );
 }
 
